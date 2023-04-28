8a8d76460a99bad3f9a7bf667f1a670fd27838a1
jusdy <pauleta0126@gmail.com>
2023-04-28 14:44:59 +0200

    add ethers

--- src/views/WelcomePage/traitITem/TraitItem.tsx
+++ src/views/WelcomePage/traitITem/TraitItem.tsx
@@ -1,8 +1,22 @@
+import { ethers } from "ethers";
+import { useWeb3React } from "@web3-react/core"
+import { trait_shop_address } from "constant/address";
+import { Contract } from '@ethersproject/contracts';
+import TraitShopABI from 'abi/TraitShop.json';
 import Button from "components/button/Button";
 
 const TraitItem = ({
     item
 }: any) => {
+    const { library } = useWeb3React();
+    const onBuyETH = () => {
+        const TraitShopContract = new Contract(
+            trait_shop_address,
+            TraitShopABI,
+            library
+        );
+
+    }
     return (
         <div className="w-[256px]">
             <img className="w-fit rounded-xl" alt="" src={item?.uri}/>
@@ -13,7 +27,7 @@ const TraitItem = ({
                 <p className="text-[#e16d2d]">#{item?.name}</p>
             </div>
 
-            <Button className="w-full text-white text-lg h-8 mb-2">Buy with ETH</Button>
+            <Button onClick={onBuyETH} className="w-full text-white text-lg h-8 mb-2">Buy with ETH</Button>
             <Button className="w-full text-white text-lg h-8">Buy with METH</Button>
         </div>
     )
